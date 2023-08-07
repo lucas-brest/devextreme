3a5c71544ac89ca9c778dd874058cd3f43fce58a
Lucas Brest <lucas.brest@moxit.com.ar>
2023-08-07 15:34:47 -0300

    Add api en backend

---
+++ back/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using back.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<User>> Get()
+        {
+            return Ok(new User());
+        }
+    }
+}
