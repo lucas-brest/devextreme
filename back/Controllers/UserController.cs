b89c935326002466eda68e093ce7ab4d72996b20
Lucas Brest <lucas.brest@moxit.com.ar>
2023-08-08 09:16:26 -0300

    Cambio en user model

--- back/Controllers/UserController.cs
+++ back/Controllers/UserController.cs
@@ -12,13 +12,18 @@ namespace back.Controllers
         private readonly List<User> _users = new List<User>
         {
             new User { UserId = 1, UserName = "User1" },
-            new User { UserId = 2, UserName = "User2" }
+            new User { UserId = 2, UserName = "User2" },
+            new User { UserId = 3, UserName = "User3" },
+            new User { UserId = 4, UserName = "User0" },
+            new User { UserId = 5, UserName = "User0" },
+            new User { UserId = 6, UserName = "User6" },
+            new User { UserId = 7, UserName = "Otro1" },
+            new User { UserId = 8, UserName = "Otro2" }
         };
 
         [HttpGet]
-        public async Task<ActionResult<List<User>>> Get()
+        public ActionResult<IQueryable<List<User>>> Get()
         {
-            Console.WriteLine("Entro");
             return Ok(_users.AsQueryable());
         }
     }
--- back/Model/User.cs
+++ back/Model/User.cs
@@ -1,8 +1,12 @@
-﻿namespace back.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace back.Model
 {
     public class User
     {
+        [Key]
         public int UserId { get; set; }
+        [Required]
         public string UserName { get; set; }
     }
 }
